5afdf2f9284e3a12288437f88d6b34ed56ccde99
Maarten Segers <maarten.segers@digipolis.gent>
2021-02-02 17:13:42 +0100

    Port to 4.x

--- src/Ruleset.php
+++ src/Ruleset.php
@@ -2,14 +2,19 @@
 
 namespace NdB\TwigCSA11Y;
 
-use Allocine\Twigcs\Ruleset\RulesetInterface;
-use Allocine\Twigcs\Validator\Violation;
+use FriendsOfTwig\Twigcs\Ruleset\RulesetInterface;
+use FriendsOfTwig\Twigcs\Validator\Violation;
 use NdB\TwigCSA11Y\Rules\AriaRoles;
 use NdB\TwigCSA11Y\Rules\BannedHTMLTags;
 use NdB\TwigCSA11Y\Rules\TabIndex;
 
 class Ruleset implements RulesetInterface
 {
+    public function __construct(int $twigMajorVersion)
+    {
+
+    }
+
     public function getRules()
     {
         return [
